eef01106a95f887bf4c549c2e93922c18ca2e211
Michał Kurczak <kurczakmichal97@gmail.com>
2024-02-24 16:59:56 +0100

    feat: add password form field widget

---
+++ lib/presentation/common/onboarding/password_form_field.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+
+class PasswordFormField extends HookWidget {
+  const PasswordFormField({
+    required this.controller,
+    required this.onChanged,
+    super.key,
+  });
+
+  final TextEditingController controller;
+  final void Function(String) onChanged;
+
+  @override
+  Widget build(BuildContext context) {
+    final isObscure = useState(true);
+    return Row(
+      children: [
+        Expanded(
+          child: TextFormField(
+            obscureText: isObscure.value,
+            controller: controller,
+            decoration: const InputDecoration(
+              labelText: 'Password',
+            ), // TODO(Kura): Localize. out-of-scope
+            onChanged: onChanged,
+          ),
+        ),
+        IconButton(
+          onPressed: () {
+            isObscure.value = !isObscure.value;
+          },
+          icon: Icon(
+            isObscure.value ? Icons.visibility_off : Icons.visibility,
+          ),
+        ),
+      ],
+    );
+  }
+}
