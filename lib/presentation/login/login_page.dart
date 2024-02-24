9b1d4398ef3b426a155cc3ce4f1425a92bcb7fc6
Michał Kurczak <kurczakmichal97@gmail.com>
2024-02-24 12:46:52 +0100

    feat: implement login page

--- lib/presentation/login/login_page.dart
+++ lib/presentation/login/login_page.dart
@@ -2,8 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_chat_app/di/injection.dart';
 import 'package:flutter_chat_app/presentation/common/form_field.dart';
+import 'package:flutter_chat_app/presentation/common/onboarding/onboarding_wrapper.dart';
 import 'package:flutter_chat_app/presentation/common/progress_indicator.dart';
 import 'package:flutter_chat_app/presentation/login/bloc/login_bloc.dart';
+import 'package:flutter_chat_app/style/app_dimens.dart';
+import 'package:flutter_chat_app/style/app_gap.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 
 class LoginPage extends HookWidget {
@@ -11,35 +14,44 @@ class LoginPage extends HookWidget {
 
   @override
   Widget build(BuildContext context) {
-    final emailController = useTextEditingController();
-    final passwordController = useTextEditingController();
     return BlocProvider<LoginBloc>(
       create: (context) => getIt<LoginBloc>(),
-      child: Scaffold(
-        body: BlocConsumer<LoginBloc, LoginState>(
-          listener: (context, state) {
-            print(state.toString());
-            emailController.text = state.email;
-            passwordController.text = state.password;
-          },
-          builder: (context, state) {
-            return state.status.map(
-              initial: (_) => _LoginForm(
-                emailController: emailController,
-                passwordController: passwordController,
-              ),
-              submitting: (_) => const ChickenProgressIndicator(),
-              success: (_) => const _SuccessWidget(),
-              failure: (_) => const _FailureWidget(),
-            );
-          },
+      child: const SafeArea(
+        child: Scaffold(
+          body: _PageBody(),
         ),
       ),
     );
   }
 }
 
-// TODO (Kura) Temp, remove
+class _PageBody extends HookWidget {
+  const _PageBody();
+
+  @override
+  Widget build(BuildContext context) {
+    final emailController = useTextEditingController();
+    final passwordController = useTextEditingController();
+    return BlocBuilder<LoginBloc, LoginState>(
+      builder: (context, state) {
+        return Padding(
+          padding: AppDimens.wrapPadding,
+          child: state.status.map(
+            initial: (_) => _LoginForm(
+              emailController: emailController,
+              passwordController: passwordController,
+            ),
+            submitting: (_) => const ChickenProgressIndicator(),
+            success: (_) => const _SuccessWidget(),
+            failure: (_) => const _FailureWidget(),
+          ),
+        );
+      },
+    );
+  }
+}
+
+// TODO(Kura): Temp, remove
 class _FailureWidget extends StatelessWidget {
   const _FailureWidget();
 
@@ -49,7 +61,7 @@ class _FailureWidget extends StatelessWidget {
   }
 }
 
-// TODO (Kura) Temp, remove
+// TODO(Kura): Temp, remove
 class _SuccessWidget extends StatelessWidget {
   const _SuccessWidget();
 
@@ -70,31 +82,39 @@ class _LoginForm extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        const Text('Login Page'), // TODO (Kura): Localize. out-of-scope
-
-        Column(
-          children: [
-            ChickenFormField(
-              labelText: 'Email',
-              // TODO (Kura): Localize. out-of-scope
-              controller: emailController,
-              onChanged: (value) => _onEmailChanged(context, value),
-            ),
-            ChickenFormField(
-              labelText: 'Password',
-              // TODO (Kura): Localize. out-of-scope
-              controller: passwordController,
-              onChanged: (value) => _onPasswordChanged(context, value),
-            ),
-            _LoginButton(
-              onPressed: () => _onSubmitted(context),
-            ),
-          ],
-        ),
-      ],
+    return OnboardingWrapper(
+      title: 'Login', // TODO: Localize. out-of-scope
+      subtitle: 'Welcome back', // TODO: Localize. out-of-scope
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Gap.listSmall,
+              ChickenFormField(
+                labelText: 'Email',
+                // TODO(Kura): Localize. out-of-scope
+                controller: emailController,
+                onChanged: (value) => _onEmailChanged(context, value),
+              ),
+              Gap.listSmall,
+              ChickenFormField(
+                labelText: 'Password',
+                // TODO(Kura): Localize. out-of-scope
+                controller: passwordController,
+                onChanged: (value) => _onPasswordChanged(context, value),
+              ),
+              Gap.listMedium,
+              _LoginButton(
+                onPressed: () => _onSubmitted(context),
+              ),
+              Gap.listMedium,
+              const _SignUpButton(),
+            ],
+          ),
+        ],
+      ),
     );
   }
 
@@ -129,7 +149,19 @@ class _LoginButton extends StatelessWidget {
   Widget build(BuildContext context) {
     return ElevatedButton(
       onPressed: onPressed,
-      child: const Text('Login'), // TODO (Kura): Localize. out-of-scope
+      child: const Text('Login'), // TODO(Kura): Localize. out-of-scope
+    );
+  }
+}
+
+class _SignUpButton extends StatelessWidget {
+  const _SignUpButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+      child: const Text('New User Sign Up'), // TODO: Localize. out-of-scope
+      onPressed: () => Navigator.of(context).pushNamed('/register'),
     );
   }
 }
