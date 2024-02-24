011b01003c0a4a971131ea2344561b5f36c4c2bb
Michał Kurczak <kurczakmichal97@gmail.com>
2024-02-24 13:03:17 +0100

    feat: add basic routing and theme

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_chat_app/di/injection.config.dart';
 import 'package:flutter_chat_app/di/injection.dart';
+import 'package:flutter_chat_app/miscellaneous/context_extension.dart';
 import 'package:flutter_chat_app/presentation/auth_bloc/auth_bloc.dart';
 import 'package:flutter_chat_app/presentation/chat/chat_page.dart';
 import 'package:flutter_chat_app/presentation/login/login_page.dart';
+import 'package:flutter_chat_app/presentation/register/register_page.dart';
+import 'package:flutter_chat_app/style/app_colors.dart';
+import 'package:flutter_chat_app/style/app_dimens.dart';
 
 void main() {
   getIt.init();
@@ -17,28 +21,58 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        body: BlocProvider(
+      home: SafeArea(
+        child: BlocProvider(
           create: (context) =>
               getIt<AuthBloc>()..add(const AuthEvent.authCheckRequested()),
           child: BlocListener<AuthBloc, AuthState>(
             listener: (context, state) {
               state.when(
-                  authenticated: () => Navigator.of(context).pushReplacement(
-                      MaterialPageRoute(
-                          builder: (context) => const ChatPage())),
-                  unauthenticated: () => Navigator.of(context).pushReplacement(
-                      MaterialPageRoute(
-                          builder: (context) => const LoginPage())));
+                authenticated: () => Navigator.of(context).pushReplacement(
+                  MaterialPageRoute(
+                    builder: (context) => const ChatPage(),
+                  ),
+                ),
+                unauthenticated: () => Navigator.of(context).pushReplacement(
+                  MaterialPageRoute(
+                    builder: (context) => const LoginPage(),
+                  ),
+                ),
+              );
             },
             child: const SizedBox(),
           ),
         ),
       ),
+      theme: ThemeData(
+        // TODO(Kura): Extract
+        inputDecorationTheme: const InputDecorationTheme(
+          filled: true,
+          fillColor: AppColors.secondaryColor,
+          border: OutlineInputBorder(
+            borderSide: BorderSide.none,
+            borderRadius:
+                BorderRadius.all(Radius.circular(AppDimens.borderRadius)),
+          ),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            textStyle: context.theme.textTheme.bodyLarge?.copyWith(
+              fontWeight: FontWeight.bold,
+            ),
+            backgroundColor: AppColors.primaryColor,
+            foregroundColor: AppColors.onPrimary,
+            padding: AppDimens.buttonPadding,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(AppDimens.borderRadius),
+            ),
+          ),
+        ),
+      ),
       routes: {
         '/login': (context) => const LoginPage(),
-        '/register': (context) => const LoginPage(),
-        '/chat': (context) => const ChatPage()
+        '/register': (context) => const RegisterPage(),
+        '/chat': (context) => const ChatPage(),
       },
     );
   }
