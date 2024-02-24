d4a4d9514889d3dabe144eb1a5c968aadc039bff
Michał Kurczak <kurczakmichal97@gmail.com>
2024-02-24 13:02:51 +0100

    chore: refactor register ui

--- lib/presentation/register/register_page.dart
+++ lib/presentation/register/register_page.dart
@@ -2,8 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_chat_app/di/injection.dart';
 import 'package:flutter_chat_app/presentation/common/form_field.dart';
+import 'package:flutter_chat_app/presentation/common/onboarding/onboarding_wrapper.dart';
 import 'package:flutter_chat_app/presentation/common/progress_indicator.dart';
 import 'package:flutter_chat_app/presentation/register/bloc/register_bloc.dart';
+import 'package:flutter_chat_app/style/app_dimens.dart';
+import 'package:flutter_chat_app/style/app_gap.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 
 class RegisterPage extends HookWidget {
@@ -14,26 +17,33 @@ class RegisterPage extends HookWidget {
     final emailController = useTextEditingController();
     final passwordController = useTextEditingController();
     final usernameController = useTextEditingController();
-    return BlocProvider(
-      create: (context) => getIt<RegisterBloc>(),
-      child: BlocConsumer<RegisterBloc, RegisterState>(
-        listener: (context, state) {
-          emailController.text = state.email;
-          passwordController.text = state.password;
-          usernameController.text = state.username;
-        },
-        builder: (context, state) {
-          return state.status.map(
-            initial: (_) => _RegisterForm(
-              emailController: emailController,
-              passwordController: passwordController,
-              usernameController: usernameController,
-            ),
-            submitting: (_) => const ChickenProgressIndicator(),
-            success: (_) => const Text('Success'),
-            failure: (_) => const Text('Failure'),
-          );
-        },
+    return SafeArea(
+      child: BlocProvider(
+        create: (context) => getIt<RegisterBloc>(),
+        child: Scaffold(
+          body: BlocConsumer<RegisterBloc, RegisterState>(
+            listener: (context, state) {
+              emailController.text = state.email;
+              passwordController.text = state.password;
+              usernameController.text = state.username;
+            },
+            builder: (context, state) {
+              return Padding(
+                padding: AppDimens.wrapPadding,
+                child: state.status.map(
+                  initial: (_) => _RegisterForm(
+                    emailController: emailController,
+                    passwordController: passwordController,
+                    usernameController: usernameController,
+                  ),
+                  submitting: (_) => const ChickenProgressIndicator(),
+                  success: (_) => const Text('Success'),
+                  failure: (_) => const Text('Failure'),
+                ),
+              );
+            },
+          ),
+        ),
       ),
     );
   }
@@ -52,37 +62,45 @@ class _RegisterForm extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        const Text('Register Page'), // TODO (Kura): Localize. out-of-scope
-
-        Column(
-          children: [
-            ChickenFormField(
-              labelText: 'Username',
-              // TODO (Kura): Localize. out-of-scope
-              controller: usernameController,
-              onChanged: (value) => _onUsernameChanged(context, value),
-            ),
-            ChickenFormField(
-              labelText: 'Email',
-              // TODO (Kura): Localize. out-of-scope
-              controller: emailController,
-              onChanged: (value) => _onEmailChanged(context, value),
-            ),
-            ChickenFormField(
-              labelText: 'Password',
-              // TODO (Kura): Localize. out-of-scope
-              controller: passwordController,
-              onChanged: (value) => _onPasswordChanged(context, value),
-            ),
-            _RegisterButton(
-              onPressed: () => _onSubmitted(context),
-            ),
-          ],
-        ),
-      ],
+    return OnboardingWrapper(
+      title: 'Register',
+      subtitle: "Welcome, let's get started",
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              ChickenFormField(
+                labelText: 'Username',
+                // TODO(Kura): Localize. out-of-scope
+                controller: usernameController,
+                onChanged: (value) => _onUsernameChanged(context, value),
+              ),
+              Gap.listSmall,
+              ChickenFormField(
+                labelText: 'Email',
+                // TODO(Kura): Localize. out-of-scope
+                controller: emailController,
+                onChanged: (value) => _onEmailChanged(context, value),
+              ),
+              Gap.listSmall,
+              ChickenFormField(
+                labelText: 'Password',
+                // TODO(Kura): Localize. out-of-scope
+                controller: passwordController,
+                onChanged: (value) => _onPasswordChanged(context, value),
+              ),
+              Gap.listMedium,
+              _RegisterButton(
+                onPressed: () => _onSubmitted(context),
+              ),
+              Gap.listSmall,
+              const _LogInButton(),
+            ],
+          ),
+        ],
+      ),
     );
   }
 
@@ -123,7 +141,21 @@ class _RegisterButton extends StatelessWidget {
   Widget build(BuildContext context) {
     return ElevatedButton(
       onPressed: onPressed,
-      child: const Text('Register'), // TODO (Kura): Localize. out-of-scope
+      child: const Text('Register'), // TODO(Kura): Localize. out-of-scope
+    );
+  }
+}
+
+class _LogInButton extends StatelessWidget {
+  const _LogInButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+      child: const Text(
+        'Already have an account? Log In',
+      ), // TODO: Localize. out-of-scope
+      onPressed: () => Navigator.of(context).pushNamed('/login'),
     );
   }
 }
